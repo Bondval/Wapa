8551afeb8c8df9ba195ec29de8bbd40168b8f929
Bond val <Bond val>
2017-08-26 21:01:47 +0300

    commit

---
+++ Scripts/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+	
+	$('#slickSlider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		focusOnSelect: true,
+		autoplay: true,
+		autoplaySpeed: 1000
+	});
+});
+$(document).ready(function () {
+	$('.booknow').click(function () {
+		$( ".booknow" ).hide()
+		$( ".box-input" ).show()
+	});
+});
